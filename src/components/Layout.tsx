c3f5ca515a87f07941e29e7312e391c6603909d5
sihoopark <sihoopark@sihooparks-MacBook-Pro.local>
2025-09-24 13:19:22 +0900

    Layout.jsx -> tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,18 @@
+import React from 'react';
 import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
 
-export default function Layout({ children }) {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <div>
       <Header />
-
       <main className="pt-14">
         {children}
       </main>
-
       <Footer />
     </div>
   );
